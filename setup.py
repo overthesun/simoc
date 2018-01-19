91ff5470062d6c0d6e090de9f8a6e7711cbbd2cf
Ben McCord <bdmccord@asu.edu>
2018-01-18 21:23:50 -0600

    Moved test to sub package to test travis setup

--- setup.py
+++ setup.py
@@ -14,5 +14,7 @@ setup (
     name="simoc_server",
     packages=["simoc_server"],
     include_package_data=True,
-    install_requires=requirements
+    install_requires=requirements,
+    test_suite="nose.collector",
+    tests_require=["nose"]
 )
