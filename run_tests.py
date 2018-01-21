a5bb7b25c4148e93556cce2693408e93cabeab10
Ben McCord <bdmccord@asu.edu>
2018-01-20 18:06:07 -0600

    Added encrypted test settings for database connection

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,9 @@ import nose
 
 
 if __name__ == "__main__":
-    os.environ["DIAG_CONFIG_MODULE"]="simoc_server.test_settings"
+    if not os.path.isfile("test_settings.py"):
+        os.environ["DIAG_CONFIG_MODULE"]="simoc_server.default_test_settings"
+    else:
+        os.environ["DIAG_CONFIG_MODULE"]="test_settings"
 
     nose.main()
--- simoc_server/test_settings.py
+++
@@ -1,3 +0,0 @@
-SECRET_KEY="$$#@AS]d##$ADVH]]3$^s&*!acgs"
-SQLALCHEMY_DATABASE_URI="postgres://postgres:pass123@192.168.1.138:5432/simoc_server_test"
-TESTING=True
