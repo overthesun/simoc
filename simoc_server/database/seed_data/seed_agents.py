c1c77e6cbf00b82e7cb4778baf2b6f3412c46bc5
tacurry3 <tacurry3@asu.edu>
2018-02-11 11:28:44 -0800

    Fixed some indentation/tab errors

--- simoc_server/database/seed_data/seed_agents.py
+++ simoc_server/database/seed_data/seed_agents.py
@@ -4,101 +4,101 @@ from simoc_server.database.db_model import AgentType, AgentTypeAttribute
 
 
 def seed():
-    human_data = gen_human()
-    plant_data = gen_plants()
-    structure_data = gen_structures()
+	human_data = gen_human()
+	plant_data = gen_plants()
+	structure_data = gen_structures()
 
-    util.add_all(human_data)
-    util.add_all(plant_data)
-    util.add_all(structure_data)
+	util.add_all(human_data)
+	util.add_all(plant_data)
+	util.add_all(structure_data)
 
 
 
 def gen_human():
-    data = OrderedDict()
-    data["human_agent_type"] = AgentType(name="human")
+	data = OrderedDict()
+	data["human_agent_type"] = AgentType(name="human")
 
-    # max energy a human can have
-    data["human_max_energy_attr"] = create_agent_type_attr(
-        data["human_agent_type"], "max_energy", 100)
+	# max energy a human can have
+	data["human_max_energy_attr"] = create_agent_type_attr(
+		data["human_agent_type"], "max_energy", 100)
 
 
-    return data
+	return data
 
 def gen_plants():
-    data = OrderedDict()
+	data = OrderedDict()
 
-    add_plant(data, "cabbage", 7.19, 9.88, 1.77, 75.78, 6.74)
-    add_plant(data, "carrot", 16.36, 22.50, 1.77, 74.83, 59.87)
-    add_plant(data, "chard", 11.49, 15.79, 1.77, 87.50, 37.69)
-    add_plant(data, "celery", 12.24, 16.83, 1.24, 103.27, 11.47)
-    add_plant(data, "cry_bean", 30.67, 42.17, 2.53, 11.11, 150.00)
-    add_plant(data, "green_onion", 10.67, 14.67, 1.74, 81.82, 10.00)
-    add_plant(data, "lettuce", 7.78, 10.70, 1.77, 131.35, 7.30)
-    add_plant(data, "pea", 12, 16.50, 1.74, 12.20, 161.00)
-    add_plant(data, "peanut", 35.84, 49.28, 2.77, 5.96, 168.75)
-    add_plant(data, "pepper", 24.71, 33.98, 1.77, 148.94, 127.43)
-    add_plant(data, "radish", 11.86, 16.31, 1.77, 91.67, 55.00)
-    add_plant(data, "red_beet", 7.11, 9.77, 1.77, 32.50, 35.00)
-    add_plant(data, "rice", 36.55, 50.26, 3.43, 10.30, 211.58)
-    add_plant(data, "snap_bean", 36.43, 50.09, 2.46, 148.50, 178.20)
-    add_plant(data, "soybean", 13.91, 19.13, 2.88, 5.04, 68.04)
-    add_plant(data, "spinach", 7.78, 10.70, 1.77, 72.97, 7.30)
-    add_plant(data, "strawberry", 25.32, 34.82, 2.22, 77.88, 144.46)
-    add_plant(data, "sweet_potato", 41.12, 56.54, 2.88, 51.72, 225.00)
-    add_plant(data, "tomato", 26.36, 36.24, 2.77, 173.76, 127.43)
-    add_plant(data, "wheat", 56, 77, 11.79, 22.73, 300.00)
-    add_plant(data, "white_potato", 32.23, 45.23, 2.88, 105.30, 90.25)
-    add_plant(data, "default_plant", 30, 40, 5, 50, 50)
+	add_plant(data, "cabbage", 7.19, 9.88, 1.77, 75.78, 6.74)
+	add_plant(data, "carrot", 16.36, 22.50, 1.77, 74.83, 59.87)
+	add_plant(data, "chard", 11.49, 15.79, 1.77, 87.50, 37.69)
+	add_plant(data, "celery", 12.24, 16.83, 1.24, 103.27, 11.47)
+	add_plant(data, "cry_bean", 30.67, 42.17, 2.53, 11.11, 150.00)
+	add_plant(data, "green_onion", 10.67, 14.67, 1.74, 81.82, 10.00)
+	add_plant(data, "lettuce", 7.78, 10.70, 1.77, 131.35, 7.30)
+	add_plant(data, "pea", 12, 16.50, 1.74, 12.20, 161.00)
+	add_plant(data, "peanut", 35.84, 49.28, 2.77, 5.96, 168.75)
+	add_plant(data, "pepper", 24.71, 33.98, 1.77, 148.94, 127.43)
+	add_plant(data, "radish", 11.86, 16.31, 1.77, 91.67, 55.00)
+	add_plant(data, "red_beet", 7.11, 9.77, 1.77, 32.50, 35.00)
+	add_plant(data, "rice", 36.55, 50.26, 3.43, 10.30, 211.58)
+	add_plant(data, "snap_bean", 36.43, 50.09, 2.46, 148.50, 178.20)
+	add_plant(data, "soybean", 13.91, 19.13, 2.88, 5.04, 68.04)
+	add_plant(data, "spinach", 7.78, 10.70, 1.77, 72.97, 7.30)
+	add_plant(data, "strawberry", 25.32, 34.82, 2.22, 77.88, 144.46)
+	add_plant(data, "sweet_potato", 41.12, 56.54, 2.88, 51.72, 225.00)
+	add_plant(data, "tomato", 26.36, 36.24, 2.77, 173.76, 127.43)
+	add_plant(data, "wheat", 56, 77, 11.79, 22.73, 300.00)
+	add_plant(data, "white_potato", 32.23, 45.23, 2.88, 105.30, 90.25)
+	add_plant(data, "default_plant", 30, 40, 5, 50, 50)
 
-    return data
+	return data
 
 
 def add_plant(data, name, oxygen, carbon, water, edible, inedible):
-    agent_type = AgentType(name=name)
-    data["{0}_plant_agent_type"] = agent_type
+	agent_type = AgentType(name=name)
+	data["{0}_plant_agent_type"] = agent_type
 
-    def add_plant_attribute(attribute_name, value, units):
-        key = "{0}_{1}_attr".format(name, attribute_name)
-        data[key] = create_agent_type_attr(agent_type, attribute_name, value, units)
+	def add_plant_attribute(attribute_name, value, units):
+		key = "{0}_{1}_attr".format(name, attribute_name)
+		data[key] = create_agent_type_attr(agent_type, attribute_name, value, units)
 
-    add_plant_attribute("oxygen_consumption", oxygen, "(g/m^2)*day")
-    add_plant_attribute("carbon_uptake", carbon, "(g/m^2)*day")
-    add_plant_attribute("water_uptake", water, "(kg/m^2)*day")
-    add_plant_attribute("edible", edible, "(g[dry weight]/m^2)*day")
-    add_plant_attribute("inedible", inedible, "(g[dry weight]/m^2)*day")
+	add_plant_attribute("oxygen_consumption", oxygen, "(g/m^2)*day")
+	add_plant_attribute("carbon_uptake", carbon, "(g/m^2)*day")
+	add_plant_attribute("water_uptake", water, "(kg/m^2)*day")
+	add_plant_attribute("edible", edible, "(g[dry weight]/m^2)*day")
+	add_plant_attribute("inedible", inedible, "(g[dry weight]/m^2)*day")
 
 def gen_structures():
-    data = OrderedDict()
+	data = OrderedDict()
 
-    add_structure(data, "Airlock", 10, 10, 10, 50, 0.9, 10, 10)
-    add_structure(data, "Crew_Quarters", 10, 10, 10, 50, 0.9, 10, 10)
-    add_structure(data, "Greenhouse", 10, 10, 10, 50, 0.9, 10, 10)
-    add_structure(data, "Kitchen", 10, 10, 10, 50, 0.9, 10, 10)
-    add_structure(data, "Power_Station", 10, 10, 10, 50, 0.9, 10, 0)
-    add_structure(data, "Rocket_Pad", 10, 10, 10, 50, 0.9, 10, 10)
-    add_structure(data, "Rover_Dock", 10, 10, 10, 50, 0.9, 10, 0)
-    add_structure(data, "Storage_Facility", 10, 10, 10, 50, 0.9, 10, 0)
+	add_structure(data, "Airlock", 10, 10, 10, 50, 0.9, 10, 10)
+	add_structure(data, "Crew_Quarters", 10, 10, 10, 50, 0.9, 10, 10)
+	add_structure(data, "Greenhouse", 10, 10, 10, 50, 0.9, 10, 10)
+	add_structure(data, "Kitchen", 10, 10, 10, 50, 0.9, 10, 10)
+	add_structure(data, "Power_Station", 10, 10, 10, 50, 0.9, 10, 0)
+	add_structure(data, "Rocket_Pad", 10, 10, 10, 50, 0.9, 10, 10)
+	add_structure(data, "Rover_Dock", 10, 10, 10, 50, 0.9, 10, 0)
+	add_structure(data, "Storage_Facility", 10, 10, 10, 50, 0.9, 10, 0)
 
-    return data
+	return data
 
-def add_structure(data, name, length, width, height, mass, efficiency, time, power_consumed)
-    agent_type = AgentType(name=name)
-    data["{0}_structure_agent_type"] = agent_type
+def add_structure(data, name, length, width, height, mass, efficiency, time, power_consumed):
+	agent_type = AgentType(name=name)
+	data["{0}_structure_agent_type"] = agent_type
 
-    def add_structure_attribute(attribute_name, value, units):
-        key = "{0}_{1}_attr".format(name, attribute_name)
-        data[key] = create_agent_type_attr(agent_type, attribute_name, value, units)
+	def add_structure_attribute(attribute_name, value, units):
+		key = "{0}_{1}_attr".format(name, attribute_name)
+		data[key] = create_agent_type_attr(agent_type, attribute_name, value, units)
 
-    add_structure_attribute("length", length, "m")
-    add_structure_attribute("width", width, "m")
-    add_structure_attribute("height", height, "m")
-    add_structure_attribute("mass", mass, "kg")
-    add_structure_attribute("efficiency", efficiency, " ")
-    add_structure_attribute("time", time, "hours")
-    add_structure_attribute("power_consumption", power, "kW/day")
+	add_structure_attribute("length", length, "m")
+	add_structure_attribute("width", width, "m")
+	add_structure_attribute("height", height, "m")
+	add_structure_attribute("mass", mass, "kg")
+	add_structure_attribute("efficiency", efficiency, " ")
+	add_structure_attribute("time", time, "hours")
+	add_structure_attribute("power_consumption", power, "kW/day")
 
 
 def create_agent_type_attr(agent_type, name, value, units=None):
-    return AgentTypeAttribute(name=name, agent_type=agent_type, value=str(value),
-        value_type=str(type(value).__name__), units=units)
+	return AgentTypeAttribute(name=name, agent_type=agent_type, value=str(value),
+		value_type=str(type(value).__name__), units=units)
