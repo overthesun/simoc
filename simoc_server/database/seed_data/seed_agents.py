a82f4ee9455e09a912b2225be1a05aa51f797550
Ben McCord <bdmccord@asu.edu>
2018-02-12 23:36:51 -0600

    Added human seed data

--- simoc_server/database/seed_data/seed_agents.py
+++ simoc_server/database/seed_data/seed_agents.py
@@ -3,6 +3,8 @@ from collections import OrderedDict
 from simoc_server.database.db_model import AgentType, AgentTypeAttribute
 
 
+UNKNOWN_UNITS = "unknown"
+
 def seed():
     human_data = gen_human()
     plant_data = gen_plants()
@@ -16,10 +18,76 @@ def gen_human():
     data = OrderedDict()
     data["human_agent_type"] = AgentType(name="human")
 
+    _type = data["human_agent_type"]
+
+    age_units = "years"
+    water_usage_units = "Kg/CrewMember*d"
+    gas_units = "kPa"
+
+
+    # specifies age limits for earth-to-colony humans
+    data["human_min_arrival_age_attr"] = create_agent_type_attr(
+            _type, "min_arrival_age", 21.0, units=age_units,
+            description="Minimum expected arrival age of a human coming"
+                "from earth")
+    data["human_max_arrival_age_attr"] = create_agent_type_attr(
+            _type, "max_arrival_age", 50.0, units=age_units,
+            description="Maximum expected arrival age of a human coming"
+                "from earth")
+
     # max energy a human can have
     data["human_max_energy_attr"] = create_agent_type_attr(
-        data["human_agent_type"], "max_energy", 100)
+            _type, "max_energy", 21.0, units="MJ",
+            description="Number of days a person can go with "
+                "food times metabolic rate in MJ")
 
+    # water usage
+    data["human_consumed_water_usage"] = create_agent_type_attr(
+            _type, "consumed_water_usage", 2.5, units=water_usage_units,
+            description="Water consumed by the human.")
+    data["human_hygiene_water_usage"] = create_agent_type_attr(
+            _type, "hygiene_water_usage", 7.17, units=water_usage_units,
+            description="Water consumed for hygiene, urinal, shower.")
+    data["human_medical_water_usage"]  = create_agent_type_attr(
+            _type, "medical_water_usage", 0.5, units=water_usage_units,
+            description="Water consumed for medical purposes")
+
+    # fatal gas levels
+    data["human_fatal_o2"] = create_agent_type_attr(
+            _type, "fatal_o2_lower", 15.7, units=gas_units,
+            description="Fatal lower limit of O2")
+    data["human_fatal_co2"] = create_agent_type_attr(
+            _type, "fatal_co2_upper", 0.53, units=gas_units,
+            description="Fatal upper limit of O2")
+
+    # Metabolism function values
+    metabolic_description_format = \
+        "{} in metabolic function: (A - (age_factor*age(years)) + B(mass_factor*mass(kg) + height_factor*height(m)))/(C * work_factor * time)"
+        
+    data["human_mmetabolism_A"] = create_agent_type_attr(
+            _type, "metabolism_A", 622.0, units=UNKNOWN_UNITS,
+            description=metabolic_description_format.format("A"))
+    data["human_metabolism_age_factor"] = create_agent_type_attr(
+            _type, "metabolism_age_factor", 9.53, units=UNKNOWN_UNITS,
+            description=metabolic_description_format.format("age_factor"))
+    data["human_metabolism_B"] = create_agent_type_attr(
+            _type, "metabolism_B", 1.25, units=UNKNOWN_UNITS,
+            description=metabolic_description_format.format("B"))
+    data["human_metabolism_mass_factor"] = create_agent_type_attr(
+            _type, "metabolism_mass_factor", 15.9, units=UNKNOWN_UNITS,
+            description=metabolic_description_format.format("mass_factor"))
+    data["human_metabolism_height_factor"] = create_agent_type_attr(
+            _type, "metabolism_height_factor", 539.6, units=UNKNOWN_UNITS,
+            description=metabolic_description_format.format("height_factor"))
+    data["human_metabolism_C"] = create_agent_type_attr(
+            _type, "metabolism_C", 0.238853e3, units=UNKNOWN_UNITS,
+            description=metabolic_description_format.format("C"))
+    data["human_metabolism_work_factor_idle"] = create_agent_type_attr(
+            _type, "metabolism_work_factor_idle", 1, units=UNKNOWN_UNITS,
+            description=metabolic_description_format.format("work_factor(while idle)"))
+    data["human_metabolism_work_factor_working"] = create_agent_type_attr(
+            _type, "metabolism_work_factor_working", 1.105, units=UNKNOWN_UNITS,
+            description=metabolic_description_format.format("work_factor(while working)"))
 
     return data
 
@@ -54,19 +122,25 @@ def gen_plants():
 
 def add_plant(data, name, oxygen, carbon, water, edible, inedible):
     agent_type = AgentType(name=name)
-    data["{0}_plant_agent_type"] = agent_type
+    data["{0}_plant_agent_type".format(name)] = agent_type
 
-    def add_plant_attribute(attribute_name, value, units):
+    def add_plant_attribute(attribute_name, value, units, description):
         key = "{0}_{1}_attr".format(name, attribute_name)
-        data[key] = create_agent_type_attr(agent_type, attribute_name, value, units)
+        data[key] = create_agent_type_attr(agent_type, attribute_name, value,
+            units=units, description=description)
 
-    add_plant_attribute("oxygen_consumption", oxygen, "(g/m^2)*day")
-    add_plant_attribute("carbon_uptake", carbon, "(g/m^2)*day")
-    add_plant_attribute("water_uptake", water, "(kg/m^2)*day")
-    add_plant_attribute("edible", edible, "(g[dry weight]/m^2)*day")
-    add_plant_attribute("inedible", inedible, "(g[dry weight]/m^2)*day")
+    add_plant_attribute("oxygen_consumption", oxygen, "(g/m^2)*day",
+        "Oxygen consumed by the plant.")
+    add_plant_attribute("carbon_uptake", carbon, "(g/m^2)*day",
+        "Carbon uptake by the plant.")
+    add_plant_attribute("water_uptake", water, "(kg/m^2)*day",
+        "Water uptake by the plant.")
+    add_plant_attribute("edible", edible, "(g[dry weight]/(m^2*day)",
+        "The edible mass created by the plant per day.")
+    add_plant_attribute("inedible", inedible, "(g[dry weight]/(m^2*day)",
+        "The inedible mass created by the plant per day.")
 
 
-def create_agent_type_attr(agent_type, name, value, units=None):
+def create_agent_type_attr(agent_type, name, value, units=None, description=None):
     return AgentTypeAttribute(name=name, agent_type=agent_type, value=str(value),
-        value_type=str(type(value).__name__), units=units)
+        value_type=str(type(value).__name__), units=units, description=description)
