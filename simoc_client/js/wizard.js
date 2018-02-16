f1f332ca3f74f97035538f3fabfd75949c842c63
Greg <gschober@asu.edu>
2018-02-16 16:03:45 -0500

    Form Completion
    Form Added. Options aren't added yet.

--- simoc_client/js/wizard.js
+++ simoc_client/js/wizard.js
@@ -1,28 +1,38 @@
 $(document).ready(function () {
     //Initialize tooltips
+
+    $('nav-tabs li.active a').next().removeClass('disabled');
+    $('tab nav-tabs li.active a').next().addClass('active');
+
     $('.nav-tabs > li a[title]').tooltip();
+
+    console.log($('nav-tabs li a.active').next().id);
     
     //Wizard
-    //When you click on a tab, if it contains disabled return false/
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+    $('.nav-tabs > li a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
         var $target = $(e.target);
     
-        if ($target.parent().hasClass('disabled')) {
+        if ($target.parent().hasClass('disabled')) {            
             return false;
         }
     });
 
     $(".next-step").click(function (e) {
+        
+        var $active = $('.nav-tabs li.active');
 
-        var $active = $('.wizard .nav-tabs li.active');
+        $active.removeClass('active');
         $active.next().removeClass('disabled');
+        $active.next().addClass('active');
+
+        console.log($active.next());
         nextTab($active);
 
     });
     $(".prev-step").click(function (e) {
 
-        var $active = $('.wizard .nav-tabs li.active');
+        var $active = $('.nav-tabs li a.active');
         prevTab($active);
 
     });
@@ -34,3 +44,4 @@ function nextTab(elem) {
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }
+
