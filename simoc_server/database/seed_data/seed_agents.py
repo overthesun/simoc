a5e4866d432de93817a815153b0b1f4b7468b869
Ben McCord <bdmccord@asu.edu>
2018-03-04 19:29:16 -0600

    Added fatal co2 for default plant, fixed o2, co2 mixup for humans, changed UKNOWN UNITS to DIMENSIONLESS

--- simoc_server/database/seed_data/seed_agents.py
+++ simoc_server/database/seed_data/seed_agents.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 from simoc_server.database.db_model import AgentType, AgentTypeAttribute
 
 
-UNKNOWN_UNITS = "unknown"
+UNITLESS = "unknown"
 
 def seed():
     human_data = gen_human()
@@ -92,10 +92,10 @@ def gen_human():
 
     # respiration
     data["human_oxygen_consumption"] = create_agent_type_attr(_type,
-        "oxygen_consumption", 1.037, units=mass_usage_units,
+        "oxygen_consumption", 0.818, units=mass_usage_units,
         description="Oxygen consumed by a human per day.")
     data["human_carbon_produced"] = create_agent_type_attr(_type,
-        "carbon_produced", 0.818, units=mass_usage_units,
+        "carbon_produced", 1.037, units=mass_usage_units,
         description="Carbon dioxide produced by a human per day.")
     # solid waste
     data["human_grey_water_solid_output"] = create_agent_type_attr(_type,
@@ -121,62 +121,63 @@ def gen_human():
         "{} in metabolic function: work_factor * time(days) * (A - (age_factor*age(years)) + B(mass_factor*mass(kg) + height_factor*height(m)))/C"
 
     data["human_metabolism_A"] = create_agent_type_attr(
-        _type, "metabolism_A", 622.0, units=UNKNOWN_UNITS,
+        _type, "metabolism_A", 622.0, units=UNITLESS,
         description=metabolic_description_format.format("A"))
     data["human_metabolism_age_factor"] = create_agent_type_attr(
-        _type, "metabolism_age_factor", 9.53, units=UNKNOWN_UNITS,
+        _type, "metabolism_age_factor", 9.53, units=UNITLESS,
         description=metabolic_description_format.format("age_factor"))
     data["human_metabolism_B"] = create_agent_type_attr(
-        _type, "metabolism_B", 1.25, units=UNKNOWN_UNITS,
+        _type, "metabolism_B", 1.25, units=UNITLESS,
         description=metabolic_description_format.format("B"))
     data["human_metabolism_mass_factor"] = create_agent_type_attr(
-        _type, "metabolism_mass_factor", 15.9, units=UNKNOWN_UNITS,
+        _type, "metabolism_mass_factor", 15.9, units=UNITLESS,
         description=metabolic_description_format.format("mass_factor"))
     data["human_metabolism_height_factor"] = create_agent_type_attr(
-        _type, "metabolism_height_factor", 539.6, units=UNKNOWN_UNITS,
+        _type, "metabolism_height_factor", 539.6, units=UNITLESS,
         description=metabolic_description_format.format("height_factor"))
     data["human_metabolism_C"] = create_agent_type_attr(
-        _type, "metabolism_C", 0.238853e3, units=UNKNOWN_UNITS,
+        _type, "metabolism_C", 0.238853e3, units=UNITLESS,
         description=metabolic_description_format.format("C"))
     data["human_metabolism_work_factor_idle"] = create_agent_type_attr(
-        _type, "metabolism_work_factor_idle", 1, units=UNKNOWN_UNITS,
+        _type, "metabolism_work_factor_idle", 1, units=UNITLESS,
         description=metabolic_description_format.format("work_factor(while idle)"))
     data["human_metabolism_work_factor_working"] = create_agent_type_attr(
-        _type, "metabolism_work_factor_working", 1.105, units=UNKNOWN_UNITS,
+        _type, "metabolism_work_factor_working", 1.105, units=UNITLESS,
         description=metabolic_description_format.format("work_factor(while working)"))
 
     return data
 
 def gen_plants():
     data = OrderedDict()
-                                      #oxygen, carbon, water, edible, inedible
-    add_plant(data, "cabbage",          7.19,    9.88, 1.77,  75.78,  6.74)
-    add_plant(data, "carrot",           16.36,  22.50, 1.77,  74.83,  59.87)
-    add_plant(data, "chard",            11.49,  15.79, 1.77,  87.50,  37.69)
-    add_plant(data, "celery",           12.24,  16.83, 1.24,  103.27, 11.47)
-    add_plant(data, "cry_bean",         30.67,  42.17, 2.53,  11.11,  150.00)
-    add_plant(data, "green_onion",      10.67,  14.67, 1.74,  81.82,  10.00)
-    add_plant(data, "lettuce",          7.78,   10.70, 1.77,  131.35, 7.30)
-    add_plant(data, "pea",              12,     16.50, 1.74,  12.20,  161.00)
-    add_plant(data, "peanut",           35.84,  49.28, 2.77,  5.96,   168.75)
-    add_plant(data, "pepper",           24.71,  33.98, 1.77,  148.94, 127.43)
-    add_plant(data, "radish",           11.86,  16.31, 1.77,  91.67,  55.00)
-    add_plant(data, "red_beet",         7.11,   9.77,  1.77,  32.50,  35.00)
-    add_plant(data, "rice",             36.55,  50.26, 3.43,  10.30,  211.58)
-    add_plant(data, "snap_bean",        36.43,  50.09, 2.46,  148.50, 178.20)
-    add_plant(data, "soybean",          13.91,  19.13, 2.88,  5.04,   68.04)
-    add_plant(data, "spinach",          7.78,   10.70, 1.77,  72.97,  7.30)
-    add_plant(data, "strawberry",       25.32,  34.82, 2.22,  77.88,  144.46)
-    add_plant(data, "sweet_potato",     41.12,  56.54, 2.88,  51.72,  225.00)
-    add_plant(data, "tomato",           26.36,  36.24, 2.77,  173.76, 127.43)
-    add_plant(data, "wheat",            56,     77,    11.79, 22.73,  300.00)
-    add_plant(data, "white_potato",     32.23,  45.23, 2.88,  105.30, 90.25)
-    add_plant(data, "default_plant",    30,     40,    5,     50,     50)
+                                      #oxygen, carbon, water, edible, inedible, growth period  fatal co2
+    add_plant(data, "cabbage",          7.19,    9.88, 1.77,  75.78,  6.74,     85.0)
+    add_plant(data, "carrot",           16.36,  22.50, 1.77,  74.83,  59.87,    75.0)
+    add_plant(data, "chard",            11.49,  15.79, 1.77,  87.50,  37.69,    45.0)
+    add_plant(data, "celery",           12.24,  16.83, 1.24,  103.27, 11.47,    75.0)
+    add_plant(data, "dry_bean",         30.67,  42.17, 2.53,  11.11,  150.00,   85.0)
+    add_plant(data, "green_onion",      10.67,  14.67, 1.74,  81.82,  10.00,    50.0)
+    add_plant(data, "lettuce",          7.78,   10.70, 1.77,  131.35, 7.30,     28.0)
+    add_plant(data, "pea",              12.0,   16.50, 1.74,  12.20,  161.00,   75.0)
+    add_plant(data, "peanut",           35.84,  49.28, 2.77,  5.96,   168.75,   104.0)
+    add_plant(data, "pepper",           24.71,  33.98, 1.77,  148.94, 127.43,   85.0)
+    add_plant(data, "radish",           11.86,  16.31, 1.77,  91.67,  55.00,    25.0)
+    add_plant(data, "red_beet",         7.11,   9.77,  1.77,  32.50,  35.00,    38.0)
+    add_plant(data, "rice",             36.55,  50.26, 3.43,  10.30,  211.58,   85.0)
+    add_plant(data, "snap_bean",        36.43,  50.09, 2.46,  148.50, 178.20,   85.0)
+    add_plant(data, "soybean",          13.91,  19.13, 2.88,  5.04,   68.04,    97.0)
+    add_plant(data, "spinach",          7.78,   10.70, 1.77,  72.97,  7.30,     30.0)
+    add_plant(data, "strawberry",       25.32,  34.82, 2.22,  77.88,  144.46,   85.0)
+    add_plant(data, "sweet_potato",     41.12,  56.54, 2.88,  51.72,  225.00,   85.0)
+    add_plant(data, "tomato",           26.36,  36.24, 2.77,  173.76, 127.43,   85.0)
+    add_plant(data, "wheat",            56.0,   77.0,  11.79, 22.73,  300.00,   79.0)
+    add_plant(data, "white_potato",     32.23,  45.23, 2.88,  105.30, 90.25,    132.0)
+    add_plant(data, "default_plant",    30.0,   40.0,  5.0,   50.0,   50.0,     85.0,           .015)
 
     return data
 
 
-def add_plant(data, name, oxygen, carbon, water, edible, inedible):
+def add_plant(data, name, oxygen, carbon, water, edible,
+        inedible, growth_period, fatal_co2_lower=None):
     agent_type = AgentType(name=name)
     data["{0}_plant_agent_type".format(name)] = agent_type
 
@@ -190,6 +191,12 @@ def add_plant(data, name, oxygen, carbon, water, edible, inedible):
         "The edible mass created by the plant per day.")
     create_agent_type_attr(agent_type, "inedible", inedible, "(g[dry weight]/(m^2*day)",
         "The inedible mass created by the plant per day.")
+    create_agent_type_attr(agent_type, "growth_period", growth_period, units="days [after planted]", 
+        description="Days until plant reaches maturity after planting.")
+
+    if fatal_co2_lower is not None:
+        create_agent_type_attr(agent_type, "fatal_co2_lower", fatal_co2_lower, units="kPa",
+            description="Lower limit on fatal co2 values.")
 
 def gen_structures():
     data = OrderedDict()
