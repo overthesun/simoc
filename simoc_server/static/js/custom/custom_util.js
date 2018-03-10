b3417a0a827e4e30503b2face00be1e2c4389d73
koulyves <ykoulidi@asu.edu>
2018-03-09 20:02:35 -0600

    Update custom_util.js
    Correct Fix for URL

--- simoc_server/static/js/custom/custom_util.js
+++ simoc_server/static/js/custom/custom_util.js
@@ -1,9 +1,7 @@
 function gotourl(url) {
-    window.location.href =  window.location.href+getport()+url;
+    window.location =  url;
 }
-function getport(){
-    if(window.location.port!="0") return ":"+window.location.port
-}
+
 function isEmpty(str) {
     return (!str || 0 === str.length);
 }
