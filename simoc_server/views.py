6fa79f5f934eaf7c5b2a58aef0f40c11f5fe9d25
Greg <gschober@asu.edu>
2018-03-14 16:00:40 -0400

    File Structure Cleanup

--- simoc_server/views.py
+++ simoc_server/views.py
@@ -30,9 +30,15 @@ def create_game_runner_manager():
 def deserialize_before_request():
     deserialize_request(request)
 
+content={'formid':'wizardform',
+'wizard':{
+'startInformation':'Welcome to SIMOC, a scalable model of an isolated, off-world community. Here you will enjoy the challenges and rewards of growing your habitat to a thriving city, or exploring the surrounding terrain with a limited crew. Whatever your mode of operation, be warned that closed ecosystems are a delicate thing, easy to unbalance and difficult to recover.Select from Play Mode or Science Run. In Play Mode you will interact regularly, making decisions that alter the course of the growth of your community. In a Science Run you will configure the model up-front and let it run its full course without interaction, then collect the data when done.',
+'configurationInformation':'Select from Preset models or Configure your own SIMOC community. The Preset models are each based upon a real-world experiment or base-line configuration. They require very little input and as such, the outcome is anticipated within a certain margin of error. These are used primarily for Science Runs. If you select to Configure your own model, you will be taken step-by-step through the panels in this Configuration Wizard.'}}
+
+
 @app.route("/")
 def home():
-    return render_template('base_template.html')
+    return render_template('base_template.html', content=content)
 
 @app.route("/loginpanel", methods=["GET"])
 def loginpanel():
