e83cb982310e2ef23a1fb8ff195edc2a95e9b499
Ben McCord <bdmccord@asu.edu>
2018-03-16 20:35:57 -0500

    Fixed ajax query for updates

--- simoc_server/static/js/updateInterval.js
+++ simoc_server/static/js/updateInterval.js
@@ -4,17 +4,26 @@ $(document).ready(function(){
     console.log("TESTING TIME");
     var updateInterval = 1000; // In Milleseconds
     var updateRunning = true;
-   
+    var step_size = 1;
+
     var updateIntevalID = setInterval(function(){ if(updateRunning){updateStep();}}, updateInterval);
 
+    var current_step = null;
+
     function updateStep(){
-        getFormatted('/get_step', function (data,status) {
+        var route = "/get_step";
+        if(current_step !== null){
+            current_step += step_size;
+            route += '?step_num=' + current_step;
+        }
+        getFormatted(route, function (data,status) {
             if (status == 'success'){ 
                 console.log("Step Updated");
                 console.log(data);
                 avg_oxygen = data.avg_oxygen_pressure;
                 avg_carbonDioxide = data.avg_carbonDioxide;
                 total_water= data.total_water;
+                current_step = data.step_num;
                 updateBarChart(avg_oxygen,avg_carbonDioxide,total_water);
             }
         });
