93fe14a99e3cfdbefbc0d1d872403e4ec170bc35
Ben McCord <bdmccord@asu.edu>
2018-03-18 17:03:22 -0500

    Made all charts update, revealed issue with scaling

--- simoc_server/static/js/dashChartBar.js
+++ simoc_server/static/js/dashChartBar.js
@@ -1,269 +1,169 @@
-new Chart(document.getElementById("food-bar-chart"), {
-    type: 'bar',
-    data: {
-      labels: ["FOOD"],
-      datasets: [
-        {
-          label: "Food(kg)",
-          backgroundColor: ["#e9f0f4"],
-          data: [10]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-      },
-      scales: {
-        xAxes: [{
-          barPercentage: 1.0,
-          ticks: {
-            autoSkip: false,
-            maxRotation: 0,
-            minRotation: 0
-          }
-        }]
-      }
-    }
-});
+const MAX_BAR_TICK_LENGTH = 4;
+const MAX_LINE_XTICK_LENGTH = 6;
+const MAX_LINE_YTICK_LENGTH = 6;
+const MAX_CHART_POINTS = 12;
 
-var oxygenBar = new Chart(document.getElementById("oxygen-bar-chart"), {
-    type: 'bar',
-    data: {
-      labels: ["OXYGEN"],
-      datasets: [
-        {
-          label: "Oxygen",
-          backgroundColor: ["#e9f0f4"],
-          data: [10]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-      },
-
-      scales: {
-        xAxes: [{
-          barPercentage: 1.0,
-          ticks: {
-            autoSkip: false,
-            maxRotation: 0,
-            minRotation: 0
-          }
-        }]
-      }
-    }
-});
-
-var carbonDioxideBar = new Chart(document.getElementById("cotwo-bar-chart"), {
-    type: 'bar',
-    data: {
-      labels: ["CO2"],
-      datasets: [
-        {
-          label: "CO2",
-          backgroundColor: ["#e9f0f4"],
-          data: [4]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-      },
-      scales: {
-        xAxes: [{
-          barPercentage: 1.0,
-          ticks: {
-            autoSkip: false,
-            maxRotation: 0,
-            minRotation: 0
-          }
-        }]
-      }
-    }
-});
-
-var waterBar = new Chart(document.getElementById("water-bar-chart"), {
-    type: 'bar',
-    data: {
-      labels: ["WATER"],
-      datasets: [
-        {
-          label: "Water(L)",
-          backgroundColor: ["#e9f0f4"],
-          data: [10]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-      },
-      scales: {
-        xAxes: [{
-          barPercentage: 1.0,
-          ticks: {
-            autoSkip: false,
-            maxRotation: 0,
-            minRotation: 0
-          }
-        }]
-      }
-    }
-});
-
-new Chart(document.getElementById("kwh-bar-chart"), {
-    type: 'bar',
-    data: {
-      labels: ["kWh"],
-      datasets: [
-        {
-          label: "kWh",
-          backgroundColor: ["#e9f0f4"],
-          data: [10]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-      },
-      scales: {
-        xAxes: [{
-          barPercentage: 1.0,
-          ticks: {
-            autoSkip: false,
-            maxRotation: 0,
-            minRotation: 0
-          }
-        }]
-      }
-    }
-});
-
-function updateBarChart(avg_oxygen,avg_carbonDioxide,total_water){
-  console.log(avg_oxygen);
-  console.log(total_water);
-  oxygenBar.data.datasets[0].data[0] = avg_oxygen;
-  oxygenBar.update();   
-
-  //carbonDioxideBar.data.datasets[0].data[0] = avg_carbonDioxide;
-  //carbonDioxideBar.update();
-
-  waterBar.data.datasets[0].data[0] = total_water;
-  waterBar.update();
-
-
-  //if(oxygenLine.data.datasets[0].data.length >= 10)
-  //  oxygenLine.data.datasets[0].data.pop();
-  
-  oxygenLine.data.datasets[0].data.unshift(avg_oxygen);
-  oxygenLine.update();
-
-  //carbonDioxideLine.data.datasets[0].data.unshift(avg_carbonDioxide);
-  //carbonDioxideLine.update();
-
-  //waterLine.data.datasets[0].data.unshift(total_water);
-  //waterLine.update();
-
+var truncateToLength = function(value, maxLength){
+  if(value === undefined)
+    return value;
+  var str = value.toString();
+  if(str.length > maxLength){
+    str = value.toPrecision(maxLength - 3);
+  }
+  else if(str.length  < maxLength){
+    str = str.padStart(maxLength, " ");
+  }
+  //console.log(str);
+  return str;
 }
 
-
-var oxygenLine = new Chart(document.getElementById("oxygen-line-chart"), {
-    type: 'line',
-    data: {
-      labels: [0,1,2,3,4,5,6,7,8,9,10,11,12],
-      datasets: [
-        {
-          label: "Oxygen Pressure",
-          borderColor: ["#e9f0f4"],
-          data: [],
-          fill: false
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        text:"Oxygen Pressure",
-        display: true,
-      }
+var baseBarChartOptions = {
+  type: 'bar',
+  data: {
+    datasets: [
+    {
+      backgroundColor: ["#e9f0f4"],
     }
-});
-
-var waterDioxideLine = new Chart(document.getElementById("water-line-chart"), {
-    type: 'line',
-    data: {
-      labels: [0,1,2,3,4,5,6,7,8,9,10,11,12],
-      datasets: [
-        {
-          label: "Water(L)",
-          borderColor: ["#e9f0f4"],
-          data: [940,955,950,980,970,960,970,980,1000],
-          fill: false
-        }
-      ]
+    ]
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
     },
-    options: {
-      legend: { display: false },
-      title: {
-        text:"Total Water(L)",
-        display: true,
-      }
-    }
-});
-
-var carbonDioxideLine = new Chart(document.getElementById("co2-line-chart"), {
-    type: 'line',
-    data: {
-      labels: [0,1,2,3,4,5,6,7,8,9,10,11,12],
-      datasets: [
-        {
-          data:[.4,.3,.2,.5,.6,.7,.4,.2],
-          label: "Carbon Dioxide Pressure",
-          borderColor: ["#e9f0f4"],
-          
-          fill: false
+    scales: {
+      xAxes: [{
+        barPercentage: 1.0,
+        ticks: {
+          autoSkip: false,
+          maxRotation: 0,
+          minRotation: 0
         }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        text:"Carbon Dioxide Pressure",
-        display: true,
-      }
-    }
-});
-
-var foodLine = new Chart(document.getElementById("food-line-chart"), {
-    type: 'line',
-    data: {
-      labels: [0,1,2,3,4,5,6,7,8,9,10,11,12],
-      datasets: [
-        {
-          label: "Total Food (kg)",
-          borderColor: ["#e9f0f4"],
-          data: [,460,440,455,470,465,485,480,490,500],
-          fill: false
+      }],
+      yAxes: [{
+        stacked: true,
+        ticks: {
+          callback:function(value){
+            return truncateToLength(value, MAX_BAR_TICK_LENGTH);
+          }
         }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        text:"Total Food (kg)",
-        display: true,
-      }
+      }],
     }
-});
+  }
+}
+
+var baseLineChartOptions = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [
+      {
+        borderColor: ["#e9f0f4"],
+        data: [],
+        fill: false
+      }
+    ]
+  },
+  options: {
+    legend: { display: false },
+    responsive:true,
+    maintainAspectRatio:false,
+    title: {
+      display: true,
+    },
+      scales: {
+      xAxes: [{
+        ticks:{
+          autoSkip: true,
+          callback: function(value){
+            return truncateToLength(value, MAX_LINE_XTICK_LENGTH);
+          }
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          autoSkip:true,
+          callback:function(value){
+            return truncateToLength(value, MAX_LINE_YTICK_LENGTH);
+          }
+        }
+      }],
+    }
+  }
+}
+
+var createSingleBarOptions = function(label, datasetLabel){
+  var options = $.extend(true, {}, baseBarChartOptions);
+  options.data.labels = [label];
+  options.data.datasets[0].label = datasetLabel;
+  options.data.datasets[0].data = [];
+  return options;
+}
+
+var createSingleLineOptions =function(datasetLabel, title){
+  var options = $.extend(true, {}, baseLineChartOptions);
+  options.data.datasets[0].label = datasetLabel;
+  options.options.title.text = title;
+  console.log(options);
+  return options;
+}
+
+var createSingleBar = function(id, label, datasetLabel){
+  return new Chart(document.getElementById(id), createSingleBarOptions(label, datasetLabel));
+}
+
+var createSingleLine = function(id, label, datasetLabel){
+  return new Chart(document.getElementById(id), createSingleLineOptions(label, datasetLabel));
+}
+
+var foodBar = createSingleBar("food-bar-chart", "FOOD", "Food (kJ)");
+var oxygenBar = createSingleBar("oxygen-bar-chart", "OXYGEN", "Oxygen (kPa")
+var carbonDioxideBar = createSingleBar("cotwo-bar-chart", "CO2", "CO2 (kPa")
+var waterBar = createSingleBar("water-bar-chart", "WATER", "Water (kg)")
+var chargeBar = createSingleBar("kwh-bar-chart", "CHARGE", "Charge (kWh)")
+
+
+var oxygenLine = createSingleLine("oxygen-line-chart", "Oxygen Pressure (kPa)", "Oxygen Pressure (kPa)");
+var waterLine = createSingleLine("water-line-chart", "Total Water (kg)", "Total Water (kg)");
+var carbonDioxideLine = createSingleLine("co2-line-chart", "Carbon Dioxide Pressure (kPa)", "Carbon Dioxide Pressure (kPa)");
+var foodLine = createSingleLine("food-line-chart", "Total Food Energy (kJ)", "Total Food Energy (kJ)");
+
+
+function updateBarChart(chart, value){
+  chart.data.datasets[0].data[0] = value;
+  chart.update();
+}
+
+function updateAllBarCharts(avgOxygen, avgCarbonDioxide, totalWater, 
+    totalFoodEnergy, totalCharge){
+
+  updateBarChart(oxygenBar, avgOxygen);
+  updateBarChart(waterBar, totalWater);
+  updateBarChart(carbonDioxideBar, avgCarbonDioxide);
+  updateBarChart(foodBar, totalFoodEnergy);
+  updateBarChart(chargeBar, totalCharge);
+}
+
+function updateLineChart(chart, stepNum, value){
+  console.log(value);
+  chart.data.datasets[0].data.push(value);
+  chart.data.labels.push(stepNum);
+  if(chart.data.datasets[0].data.length > MAX_CHART_POINTS){
+    for(var i = 0; i < MAX_CHART_POINTS/3; i++){
+      chart.data.datasets[0].data.shift();
+      chart.data.labels.shift();
+    }
+  }
+  chart.update();
+}
+
+var updateAllLineCharts = function(avgOxygen, avgCarbonDioxide, 
+    totalWater, totalFoodEnergy, stepNum){
+  updateLineChart(oxygenLine, stepNum, avgOxygen);
+  updateLineChart(carbonDioxideLine, stepNum, avgCarbonDioxide);
+  updateLineChart(waterLine, stepNum, totalWater);
+  updateLineChart(foodLine, stepNum, totalFoodEnergy);
+}
+
 
 Chart.defaults.global.defaultFontColor = "#fff";
--- simoc_server/static/js/updateInterval.js
+++ simoc_server/static/js/updateInterval.js
@@ -7,31 +7,35 @@ $(document).ready(function(){
     console.log("TESTING TIME");
     var updateInterval = 1000; // In Milleseconds
     var updateRunning = true;
-    var step_size = 1;
+    var stepSize = 1;
 
     var updateIntevalID = setInterval(function(){ if(updateRunning){updateStep();}}, updateInterval);
 
 
-    var current_step = null;
+    var currentStep = null;
 
     function updateStep(){
         var route = "/get_step";
-        if(current_step !== null){
-            current_step += step_size;
-            route += '?step_num=' + current_step;
+        if(currentStep !== null){
+            currentStep += stepSize;
+            route += '?step_num=' + currentStep;
         }
         getFormatted(route, function (data,status) {
 
             if (status == 'success'){ 
                 console.log("Step Updated");
                 console.log(data);
-                avg_oxygen = data.avg_oxygen_pressure;
-                avg_carbonDioxide = data.avg_carbonDioxide;
-                total_water= data.total_water;
-                current_step = data.step_num;
-                updateBarChart(avg_oxygen,avg_carbonDioxide,total_water);
+                avgOxygen = data.avg_oxygen_pressure;
+                avgCarbonDioxide = data.avg_carbon_dioxide_pressure;
+                totalWater= data.total_water;
+                totalFoodEnergy = data.total_food_energy;
+                totalPowerCharge = data.total_power_charge;
+                currentStep = data.step_num;
+                updateAllBarCharts(avgOxygen,avgCarbonDioxide,totalWater,
+                    totalFoodEnergy, totalPowerCharge);
+                updateAllLineCharts(avgOxygen, avgCarbonDioxide, totalWater,
+                    totalFoodEnergy, currentStep);
                 //updateSolarDay(data.step.num)
-                
             }
         });
      
