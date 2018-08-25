f1cdfeec43c5ef3eac618fc6a3a77c1556ce9af8
Iurii Milovanov <duruku@gmail.com>
2018-08-25 18:08:27 -0500

    initial gcp+k8s commit

--- simoc_server/__init__.py
+++ simoc_server/__init__.py
@@ -14,6 +14,19 @@ else:
 app = Flask(__name__)
 
 app.config.from_object(config_obj)
+
+db_user = os.environ.get("DB_USER")
+db_password = os.environ.get("DB_PASSWORD")
+db_name = db_user
+
+SQLALCHEMY_DATABASE_URI = (
+    'mysql+mysqldb://{user}:{password}@mysqldb.default.svc.cluster.local:3306/{database}').format(
+        user=db_user, password=db_password,
+        database=db_name)
+print(SQLALCHEMY_DATABASE_URI)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
 db = SQLAlchemy(app, session_options={
     "expire_on_commit": False
     })
