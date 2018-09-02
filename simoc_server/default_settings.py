093dfa0d4156068bb4a88a8394811a914250990f
Iurii Milovanov <duruku@gmail.com>
2018-09-02 14:30:59 -0500

    removed temporal comments

--- simoc_server/default_settings.py
+++ simoc_server/default_settings.py
@@ -1,5 +1,2 @@
 SECRET_KEY="$$#@AS]d##$ADVH]]3$^s&*!acgs"
-# SQLALCHEMY_DATABASE_URI='sqlite:///db.sqlite'
-# SQLALCHEMY_DATABASE_URI='mysql://admin:admin@35.194.16.69/simoc'
-# SQLALCHEMY_DATABASE_URI='mysql://admin:admin@127.0.0.1:3306/simoc'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
