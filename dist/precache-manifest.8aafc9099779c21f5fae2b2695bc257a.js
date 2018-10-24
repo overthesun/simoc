45ead3aabf5d1cd8825efdd24e0fe92a8fbfef13
gschober <gschober@asu.edu>
2018-10-23 22:53:41 -0400

    Registration Fix
    Fixed Regististration to work properly

---
+++ dist/precache-manifest.8aafc9099779c21f5fae2b2695bc257a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "94a0f1452b604a7359a6",
+    "url": "/static/css/app.399b165f.css"
+  },
+  {
+    "revision": "94a0f1452b604a7359a6",
+    "url": "/static/js/app.9e4ed6d9.js"
+  },
+  {
+    "revision": "e8274ddfc02c807a9570",
+    "url": "/static/js/chunk-vendors.5b8487a4.js"
+  },
+  {
+    "revision": "eadfc587f51f905d8e270084b83e71ea",
+    "url": "/static/img/logo-vertical.eadfc587.png"
+  },
+  {
+    "revision": "a69b812a7405ae381f22bd3759b83ee4",
+    "url": "/static/img/background-mars.a69b812a.png"
+  },
+  {
+    "revision": "74b89dfe66ea52b4bd84693850265be6",
+    "url": "/static/img/background-panel-dark.74b89dfe.png"
+  },
+  {
+    "revision": "b6b49c74cf81c5564357285f53435f4c",
+    "url": "/static/img/logo-horizontal.b6b49c74.png"
+  },
+  {
+    "revision": "e84356af37a3836389a840647f8ba81a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
