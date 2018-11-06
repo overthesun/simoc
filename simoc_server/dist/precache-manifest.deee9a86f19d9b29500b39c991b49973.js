2da0f24251d4f509b42f7d057085ad2ba4f3dc1a
gschober <gschober@asu.edu>
2018-11-06 14:55:15 -0500

    Fixed Ignore File

---
+++ simoc_server/dist/precache-manifest.deee9a86f19d9b29500b39c991b49973.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b6cd0e76cfefb9df045",
+    "url": "/static/css/app.69fcf827.css"
+  },
+  {
+    "revision": "7b6cd0e76cfefb9df045",
+    "url": "/static/js/app.14869903.js"
+  },
+  {
+    "revision": "3e0e407e0189462ccba8",
+    "url": "/static/css/chunk-vendors.af6fe982.css"
+  },
+  {
+    "revision": "3e0e407e0189462ccba8",
+    "url": "/static/js/chunk-vendors.fcab170f.js"
+  },
+  {
+    "revision": "eadfc587f51f905d8e270084b83e71ea",
+    "url": "/static/img/logo-vertical.eadfc587.png"
+  },
+  {
+    "revision": "45e8fdeefaecb02c414028de0ed38c6a",
+    "url": "/static/fonts/nasalization-rg-webfont.45e8fdee.woff"
+  },
+  {
+    "revision": "a69b812a7405ae381f22bd3759b83ee4",
+    "url": "/static/img/background-mars.a69b812a.png"
+  },
+  {
+    "revision": "74b89dfe66ea52b4bd84693850265be6",
+    "url": "/static/img/background-panel-dark.74b89dfe.png"
+  },
+  {
+    "revision": "b6b49c74cf81c5564357285f53435f4c",
+    "url": "/static/img/logo-horizontal.b6b49c74.png"
+  },
+  {
+    "revision": "320e5bb1e103e5cb55204eca4f751b94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
