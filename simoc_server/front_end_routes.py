65980ac5319ea52fccaa9856c28386500cf77e50
granawkins <granthawkins88@gmail.com>
2023-01-04 12:52:41 +0700

    updates to receive b2 config from frontend

--- simoc_server/front_end_routes.py
+++ simoc_server/front_end_routes.py
@@ -216,6 +216,7 @@ def convert_configuration(game_config, agent_desc=None, save_output=False):
     # Initialize supplemental agent_desc and agent_conn
     user_agent_desc = {}
     user_agent_conn = []
+    is_b2 = False
 
     ###########################################################################
     #                   STEP 1: Add non-agent fields                          #
@@ -227,6 +228,10 @@ def convert_configuration(game_config, agent_desc=None, save_output=False):
     else:
         single_agent = 0
     full_game_config['single_agent'] = single_agent
+    # Sets the start date of the simulation
+    if 'startTime' in working_config and isinstance(working_config['startTime'], str):
+        startTime = working_config.pop('startTime')
+        full_game_config['start_time'] = startTime
     # Sets the length of the simulation
     if 'duration' in working_config and isinstance(working_config['duration'], dict):
         duration = working_config.pop('duration')
@@ -234,8 +239,11 @@ def convert_configuration(game_config, agent_desc=None, save_output=False):
             'condition': 'time',
             'value': duration.get('value', 0) or 0,
             'unit': duration.get('type', 'day')})
+    location = working_config.pop('location')
+    is_b2 = location == 'b2'
+    full_game_config['location'] == 'earth' if is_b2 else 'mars'
     # Optional fields
-    for label in ['priorities', 'minutes_per_step', 'location']:
+    for label in ['priorities', 'minutes_per_step']:
         if label in working_config and isinstance(working_config[label], dict):
             full_game_config[label] = working_config.pop(label)
     # Structures MUST step before other agents so that the `.._ratio` fields
@@ -255,7 +263,7 @@ def convert_configuration(game_config, agent_desc=None, save_output=False):
 
     # Structures: A string under 'habitat' and 'greenhouse' with selected type.
     structures_dict = {}  # Used to replace generic connections
-    total_volume = 0  # Used to calcualte starting water_storage
+    total_volume = 0  # Used to calculate starting water_storage
     if 'habitat' in working_config or 'greenhouse' in working_config:
         valid_structures = list(agent_desc['structures'].keys())
         for structure in ['habitat', 'greenhouse']:
@@ -272,15 +280,88 @@ def convert_configuration(game_config, agent_desc=None, save_output=False):
             total_volume += volume  # Used below to calculate starting water
             atmosphere = calc_air_storage(volume)  # Fill with earth-normal atmosphere
             working_config[structure_type] = dict(id=1, amount=1, **atmosphere)
-    if 'habitat' in structures_dict and 'greenhouse' in structures_dict:
-        working_config['atmosphere_equalizer'] = dict(id=1, amount=1)
-    # Default Storages: Some listed, some not. Need to calculate amount.
+    if is_b2:
+        for agent in {'b2_biomes', 'atmosphere_equalizer_gh_biomes', 'atmosphere_equalizer_gh_crew'}:
+            working_config[agent] = dict(id=1, amount=1)
+    else:
+        if 'habitat' in structures_dict and 'greenhouse' in structures_dict:
+            working_config['atmosphere_equalizer'] = dict(id=1, amount=1)
+
+    # Plants: A list of objects with 'species' and 'amount'
+    plants_in_config = []
+    input_food_storage = working_config.pop('food_storage', None)
+    crop_mgmt_input = working_config.pop('improvedCropManagement', False)
+    crop_mgmt_factor = 1.5 if crop_mgmt_input is True else 1
+    crop_mgmt_char = {'type': 'density_factor', 'value': crop_mgmt_factor}
+    density_char = {'type': 'density_factor', 'value': 0.5}
+    if 'plants' in working_config and isinstance(working_config['plants'], list):
+        plants = working_config.pop('plants')
+        for plant in plants:
+            if isinstance(plant, dict):
+                amount = plant.get('amount', 0) or 0
+                plant_type = plant.get('species', None)
+                if plant_type and amount:
+                    plants_in_config.append(plant_type)
+                    working_config[plant_type] = dict(amount=amount)
+                    if is_b2:
+                        plant_desc = copy.deepcopy(agent_desc['plants'][plant_type])
+                        plant_desc['data']['characteristics'] += [density_char, crop_mgmt_char]
+                        if 'plants' not in user_agent_desc:
+                            user_agent_desc['plants'] = {}
+                        user_agent_desc['plants'][plant_type] = plant_desc
+    if len(plants_in_config) > 0:
+        food_storage = {}
+        food_storage_desc = agent_desc['storage']['food_storage']
+        for char in food_storage_desc['data']['characteristics']:
+            if char['type'].startswith('capacity'):
+                currency = char['type'].split('_', 1)[1]
+                if currency in plants_in_config:
+                    food_storage[currency] = 0
+        if len(food_storage.keys()) > 0:
+            working_config['food_storage'] = dict(id=1, amount=1, **food_storage)
+        # Lights
+        if is_b2:
+            working_config['b2_sun'] = {'amount': 1}
+        else:
+            # Replace generic lamp with species-specific lamp
+            if 'lamp' in working_config:
+                del working_config['lamp']
+            user_agent_desc['structures'] = {}
+            for species in plants_in_config:
+                new_lamp_agent = copy.deepcopy(agent_desc['structures']['lamp'])
+                # Set baseline PAR equal to species' baseline PAR
+                species_desc = agent_desc['plants'][species]
+                for i, char in enumerate(species_desc['data']['characteristics']):
+                    if char['type'] == 'par_baseline':
+                        par_baseline = char['value']
+                        break
+                for i, char in enumerate(new_lamp_agent['data']['characteristics']):
+                    if char['type'] == 'par_baseline':
+                        new_lamp_agent['data']['characteristics'][i]['value'] = par_baseline
+                        break
+                # Set amount equal to number of species
+                species_amount = working_config[species]['amount']
+                working_config[f'{species}_lamp'] = {'amount': species_amount}
+                # Add a custom agent_desc and agent_conn for new lamp
+                user_agent_desc['structures'][f'{species}_lamp'] = new_lamp_agent
+                user_agent_conn += [
+                    {'from': 'power_storage.kwh', 'to': f'{species}_lamp.kwh'},
+                    {'from': f'{species}_lamp.par', 'to': f'{species}_lamp.par'},
+                    {'from': f'{species}_lamp.par', 'to': f'{species}.par'},
+                ]
+     # Default Storages: Some listed, some not. Need to calculate amount.
     # 'food_storage' now holds fresh food, and 'ration_storage' holds the rations. Rations are
     # still pre-loaded to 'food_storage' on the front-end though, so need to change the label.
-    if 'food_storage' in working_config:
-        storage = working_config.pop('food_storage')
-        working_config['ration_storage'] = storage
-    for storage_type in ['water_storage', 'nutrient_storage', 'ration_storage', 'power_storage']:
+    if input_food_storage and input_food_storage.get('ration') is not None:                                                                # b2: initialize with food (plants) instead of rations
+        if is_b2:
+            starting_food = input_food_storage['ration']
+            greenhouse_layout = {p: working_config[p]['amount'] for p in plants_in_config}
+            total_crop_area = sum(greenhouse_layout.values())
+            starting_food_storage = {k: starting_food*(v/total_crop_area) for k, v in greenhouse_layout.items()}
+            working_config['food_storage'].update(starting_food_storage)
+        else:
+            working_config['ration_storage'] = input_food_storage
+    for storage_type in ['water_storage', 'nutrient_storage', 'power_storage']:
         if storage_type in working_config and isinstance(working_config[storage_type], dict):
             storage = working_config.pop(storage_type)
         else:
@@ -302,86 +383,95 @@ def convert_configuration(game_config, agent_desc=None, save_output=False):
                 capacity = int(char['value'])
                 amount = max(amount, math.ceil(storage[currency] / capacity))
         working_config[storage_type] = dict(id=1, amount=amount, **storage)
-    # Plants: A list of objects with 'species' and 'amount'
-    plants_in_config = []
-    if 'plants' in working_config and isinstance(working_config['plants'], list):
-        plants = working_config.pop('plants')
-        for plant in plants:
-            if isinstance(plant, dict):
-                amount = plant.get('amount', 0) or 0
-                plant_type = plant.get('species', None)
-                if plant_type and amount:
-                    plants_in_config.append(plant_type)
-                    working_config[plant_type] = dict(amount=amount)
-    if len(plants_in_config) > 0:
-        food_storage = {}
-        food_storage_desc = agent_desc['storage']['food_storage']
-        for char in food_storage_desc['data']['characteristics']:
-            if char['type'].startswith('capacity'):
-                currency = char['type'].split('_', 1)[1]
-                if currency in plants_in_config:
-                    food_storage[currency] = 0
-        if len(food_storage.keys()) > 0:
-            working_config['food_storage'] = dict(id=1, amount=1, **food_storage)
-
-        # Replace generic lamp with species-specific lamps
-        if 'lamp' in working_config:
-            del working_config['lamp']
-        user_agent_desc['structures'] = {}
-        for species in plants_in_config:
-            new_lamp_agent = copy.deepcopy(agent_desc['structures']['lamp'])
-            # Set baseline PAR equal to species' baseline PAR
-            species_desc = agent_desc['plants'][species]
-            for i, char in enumerate(species_desc['data']['characteristics']):
-                if char['type'] == 'par_baseline':
-                    par_baseline = char['value']
-                    break
-            for i, char in enumerate(new_lamp_agent['data']['characteristics']):
-                if char['type'] == 'par_baseline':
-                    new_lamp_agent['data']['characteristics'][i]['value'] = par_baseline
-                    break
-            # Set amount equal to number of species
-            species_amount = working_config[species]['amount']
-            working_config[f'{species}_lamp'] = {'amount': species_amount}
-            # Add a custom agent_desc and agent_conn for new lamp
-            user_agent_desc['structures'][f'{species}_lamp'] = new_lamp_agent
-            user_agent_conn += [
-                {'from': 'power_storage.kwh', 'to': f'{species}_lamp.kwh'},
-                {'from': f'{species}_lamp.par', 'to': f'{species}_lamp.par'},
-                {'from': f'{species}_lamp.par', 'to': f'{species}.par'},
-            ]
-    # ECLSS: A single item with an amount; needs to be broken into component agents
-    eclss_agents = ['solid_waste_aerobic_bioreactor', 'multifiltration_purifier_post_treatment',
-                    'oxygen_generation_SFWE', 'urine_recycling_processor_VCD', 'co2_removal_SAWD',
-                    'co2_makeup_valve', 'co2_storage', 'co2_reduction_sabatier',
-                    'ch4_removal_agent', 'dehumidifier']
     if 'eclss' in working_config and isinstance(working_config['eclss'], dict):
         eclss = working_config.pop('eclss')
-        amount = eclss.get('amount', 0) or 0
-        if amount:
-            plant_area = (0 if not plants_in_config else
-                          sum([working_config[p]['amount'] for p in plants_in_config]))
-            human_amount = working_config['human_agent']['amount']
-            for eclss_agent in eclss_agents:
-                this_amount = amount
-                # Scale certain components based on plant area
-                scale_with_plants = dict(  # Based on minimum required for 4hg preset
-                    multifiltration_purifier_post_treatment=3/200,
-                    co2_makeup_valve=4/200,
-                    dehumidifier=5/200,
-                )
-                if eclss_agent in scale_with_plants:
-                    required_amount = round(scale_with_plants[eclss_agent] * plant_area)
-                    this_amount = max(this_amount, required_amount)
-                # Scale certain components based on number of humans
-                scale_with_humans = dict(
-                    co2_removal_SAWD=2/4,
-                )
-                if eclss_agent in scale_with_humans:
-                    required_amount = round(scale_with_humans[eclss_agent] * human_amount)
-                    this_amount = max(this_amount, required_amount)
-                working_config[eclss_agent] = dict(id=1, amount=this_amount)
+        if is_b2:
+            eclss_agents = {
+                'solid_waste_aerobic_bioreactor': 1,
+                'urine_recycling_processor_VCD': 1,
+                'multifiltration_purifier_post_treatment': 50,
+                'co2_removal_SAWD': 5,
+                'co2_makeup_valve': 5,
+                'dehumidifier': 50,
+                'o2_makeup_valve': 180,
+            }
+            for agent, amount in eclss_agents.items():
+                working_config[agent] = {'id': 1, 'amount': amount}
+            user_agent_desc['eclss'] = {}
+            # CO2 Control System
+            co2UpperLimit = eclss.get('co2UpperLimit', None)
+            if co2UpperLimit is not None and 'co2_removal_SAWD' in working_config:
+                decimal = round(co2UpperLimit/100, 4)
+                sawd_desc = copy.deepcopy(agent_desc['eclss']['co2_removal_SAWD'])
+                for i, flow in enumerate(sawd_desc['data']['input']):
+                    if flow['type'] == 'co2':
+                        sawd_desc['data']['input'][i]['criteria']['value'] = decimal
+                user_agent_desc['eclss']['co2_removal_SAWD'] = sawd_desc
+            co2Reserves = eclss.get('co2Reserves', None)
+            if co2Reserves is not None and 'co2_storage' in working_config:
+                working_config['co2_storage']['co2'] = co2Reserves
+            co2LowerLimit = eclss.get('co2LowerLimit', None)
+            if co2LowerLimit is not None and 'co2_makeup_valve' in working_config:
+                decimal = round(co2LowerLimit, 4)
+                valve_desc = copy.deepcopy(agent_desc['eclss']['co2_makeup_valve'])
+                for i, flow in enumerate(valve_desc['data']['input']):
+                    if flow['type'] == 'co2':
+                        valve_desc['data']['input'][i]['criteria']['value'] = decimal
+                user_agent_desc['eclss']['co2_makeup_valve'] = valve_desc
+            # O2 Control System
+            o2Reserves = eclss.get('o2Reserves', None)
+            if o2Reserves is not None and 'o2_storage' in working_config:
+                working_config['o2_storage']['o2'] = o2Reserves
+            o2LowerLimit = eclss.get('o2LowerLimit', None)
+            if o2LowerLimit is not None and 'o2_makeup_valve' in working_config:
+                decimal = round(o2LowerLimit, 4)
+                valve_desc = copy.deepcopy(agent_desc['eclss']['o2_makeup_valve'])
+                for i, flow in enumerate(valve_desc['data']['input']):
+                    if flow['type'] == 'o2':
+                        valve_desc['data']['input'][i]['criteria']['value'] = decimal
+                user_agent_desc['eclss']['o2_makeup_valve'] = valve_desc
+
+        else:
+            # ECLSS: A single item with an amount; needs to be broken into component agents
+            eclss_agents = ['solid_waste_aerobic_bioreactor', 'multifiltration_purifier_post_treatment',
+                            'oxygen_generation_SFWE', 'urine_recycling_processor_VCD', 'co2_removal_SAWD',
+                            'co2_makeup_valve', 'co2_storage', 'co2_reduction_sabatier',
+                            'ch4_removal_agent', 'dehumidifier']
+            amount = eclss.get('amount', 0) or 0
+            if amount:
+                plant_area = (0 if not plants_in_config else
+                            sum([working_config[p]['amount'] for p in plants_in_config]))
+                human_amount = working_config['human_agent']['amount']
+                for eclss_agent in eclss_agents:
+                    this_amount = amount
+                    # Scale certain components based on plant area
+                    scale_with_plants = dict(  # Based on minimum required for 4hg preset
+                        multifiltration_purifier_post_treatment=3/200,
+                        co2_makeup_valve=4/200,
+                        dehumidifier=5/200,
+                    )
+                    if eclss_agent in scale_with_plants:
+                        required_amount = round(scale_with_plants[eclss_agent] * plant_area)
+                        this_amount = max(this_amount, required_amount)
+                    # Scale certain components based on number of humans
+                    scale_with_humans = dict(
+                        co2_removal_SAWD=2/4,
+                    )
+                    if eclss_agent in scale_with_humans:
+                        required_amount = round(scale_with_humans[eclss_agent] * human_amount)
+                        this_amount = max(this_amount, required_amount)
+                    working_config[eclss_agent] = dict(id=1, amount=this_amount)
     # 'human_agent' and 'solar_pv...' are already in the correct format.
+
+    # Update humans to reduce food consumption by 50%
+    if is_b2:
+        user_agent_desc['inhabitants'] = {}
+        human_desc = copy.deepcopy(agent_desc['inhabitants']['human_agent'])
+        for i, flow in enumerate(human_desc['data']['input']):
+            if flow['type'] == 'food':
+                human_desc['data']['input'][i]['value'] *= 0.5  # ADJUST
+        user_agent_desc['inhabitants']['human_inhabitant'] = human_desc
+
 
     ###########################################################################
     #                   STEP 3: Add all agents to output                      #
