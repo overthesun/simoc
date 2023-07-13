f4a5c0a4bee1d0acc6d59213a9c3c4547ca47c23
Ezio Melotti <ezio.melotti@gmail.com>
2023-07-14 01:59:30 +0200

    Exclude obsolete doc files.

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,10 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
+exclude_patterns = [
+    '_build', 'Thumbs.db', '.DS_Store', 'venv',
+    'developer/local-deployment.rst', 'developer/docker-deployment.rst',
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
