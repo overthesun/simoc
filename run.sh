075dbf36d0b36fbd86c3acfc4df46197f3affd2b
Ezio Melotti <ezio.melotti@gmail.com>
2023-11-11 04:27:20 +0100

    Update `gunicorn` command line in `run.sh`.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn -w 1 -k gevent -b 0.0.0.0:8080 --log-level debug --timeout 120 simoc_server:app
+gunicorn -w 1 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -b 0.0.0.0:8080 --log-level debug --timeout 120 simoc_server:app
