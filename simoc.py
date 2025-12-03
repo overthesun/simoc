fb3f297e5fffaf32b0abadad52b4e6721950d1c6
Ezio Melotti <ezio.melotti@gmail.com>
2025-12-03 20:09:20 +0800

    Support different project names

--- simoc.py
+++ simoc.py
@@ -29,6 +29,7 @@ COMPOSE_FILE = 'docker-compose.mysql.yml'
 DEV_BE_COMPOSE_FILE = 'docker-compose.dev-be.yml'
 AGENT_DESC_COMPOSE_FILE = 'docker-compose.agent-desc.yml'
 TESTING_COMPOSE_FILE = 'docker-compose.testing.yml'
+PROJECT_NAME = 'simoc'  # used to handle multiple deployment
 DOCKER_COMPOSE_CMD = ['docker', 'compose', '-f', COMPOSE_FILE]
 
 
@@ -302,7 +303,7 @@ def init_db():
 def remove_db():
     """Remove the volume for the MySQL DB."""
     docker_compose('rm', '--stop', '-v', '-f', 'simoc-db')
-    docker('volume', 'rm', 'simoc_db-data')
+    docker('volume', 'rm', f'{PROJECT_NAME}_db-data')
     return True  # the volume rm might return False if the volume is missing
 
 
@@ -404,7 +405,8 @@ def test(*args):
     # add the testing yml that replaces the db
     DOCKER_COMPOSE_CMD.extend(['-f', TESTING_COMPOSE_FILE])
     # check if the volume already exists
-    cp = subprocess.run(['docker', 'volume', 'inspect', 'simoc_db-testing'],
+    test_volume = f'{PROJECT_NAME}_db-testing'
+    cp = subprocess.run(['docker', 'volume', 'inspect', test_volume],
                         capture_output=True)
     vol_info = json.loads(cp.stdout)
     if not vol_info:
@@ -432,14 +434,17 @@ def adminer(db=None):
         DOCKER_COMPOSE_CMD.extend(['-f', TESTING_COMPOSE_FILE])
     def show_info():
         # show the volume that is currently connected to the db container
+        db_container = f'{PROJECT_NAME}-simoc-db-1'
         cp = subprocess.run(['docker', 'inspect', '-f',
                              '{{range .Mounts}}{{.Name}}{{end}}',
-                             'simoc_simoc-db_1'], capture_output=True)
+                             db_container], capture_output=True)
         print('* Starting adminer at: http://localhost:8081/')
         print('* Connecting to:', cp.stdout.decode('utf-8'))
         return True
-    cmd = ['run', '--network', 'simoc_simoc-net',
-           '--link', 'simoc_simoc-db_1:db', '-p', '8081:8080',
+    network_name = f'{PROJECT_NAME}_simoc-net'
+    db_container = f'{PROJECT_NAME}-simoc-db-1'
+    cmd = ['run', '--network', network_name,
+           '--link', f'{db_container}:db', '-p', '8081:8080',
            '-e', 'ADMINER_DESIGN=dracula', 'adminer']
     return up() and show_info() and docker(*cmd)
 
@@ -509,6 +514,10 @@ Use the `--with-dev-backend` flag to run the dev backend container.
         help='the env file (default: %(default)r)'
     )
     parser.add_argument(
+        '--project-name', metavar='NAME',
+        help='the project name for docker compose'
+    )
+    parser.add_argument(
         '--with-dev-backend', action='store_true',
         help='use the dev backend flask container'
     )
@@ -532,6 +541,13 @@ Use the `--with-dev-backend` flag to run the dev backend container.
     if args.env_file:
         ENV_FILE = args.env_file
     ENVVARS = update_env(ENV_FILE)
+
+    if args.project_name:  # use project name if specified
+        PROJECT_NAME = args.project_name
+    elif args.env_file:  # or use env file name if specified
+        PROJECT_NAME = args.env_file.stem
+    # or use default 'simoc'
+    DOCKER_COMPOSE_CMD = ['docker', 'compose', '-f', COMPOSE_FILE, '-p', PROJECT_NAME]
 
     if args.dev_backend_yml and not args.with_dev_backend:
         parser.error("Can't specify the dev backend yml without --with-dev-backend")
